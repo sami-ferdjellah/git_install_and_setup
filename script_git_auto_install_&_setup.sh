ca219a5bd6e880bc9f7348ed5759a3e6c7b7893b
Sami_F <sami.ferdjellah@live.fr>
2025-01-13 21:25:27 +0100

    Create script_git_auto_install_&_setup.sh
    first iteration V1

---
+++ script_git_auto_install_&_setup.sh
@@ -0,0 +1,22 @@
+# main script git_auto_install_&_setup
+
+# instalation
+version = git --version
+if $version == true
+then
+    echo "git is already instaled"
+else
+    sudo apt update
+    sudo apt install git
+    echo "git has been installed"
+fi
+
+# setup
+gitconfig = git config --list
+if gitconfig == true
+then
+    echo "git setup has been already done"
+else
+    git config --global user.name "Sami_F"
+    git config --global user.email "sami.ferdjellah@live.fr"
+    echo "your git has been setup"
